12dbea7bc7ae08cce65bf29cc5de7cea0eefff69
MatteoDynamics <matteotennisboy@gmail.com>
2023-01-28 19:50:14 +0100

    added sorting and searsching

--- shop.c
+++ shop.c
@@ -4,16 +4,16 @@
 #include <time.h>
 #include <stdlib.h>
 
-#define NAME_COMPARE "name"
-#define SURNAME_COMPARE "surname"
-#define GROUP_COMPARE "group"
+#define NAME_COMPARE 1
+#define BRAND_COMPARE 2
+#define PRICE_COMPARE 3
 
 
 typedef struct ElectronicItem {
     char name[50];
     char brand[20];
     char model[20];
-    char price[5];
+    float price;
     char stock[5];
 }ElectronicItem;
 
@@ -108,7 +108,7 @@ void read_from_csv(node **lista)
             if (i == 0) strcpy(dane.name, field);
             else if (i == 1) strcpy(dane.brand, field);
             else if (i == 2) strcpy(dane.model, field);
-            else if (i == 3) strcpy(dane.price, field);
+            else if (i == 3) dane.price = atof(field);
             else if (i == 4) strcpy(dane.stock, field);
 
             field = strtok(NULL, ";");
@@ -126,15 +126,136 @@ void print_list(node *lista) {
     lista = lista->prev;
   }
   while (lista != NULL) {
-    printf("name: %-20s brand: %-20s model: %-20s price: %-5s stock: %-5s\n", lista->data.name, lista->data.brand, lista->data.model, lista->data.price, lista->data.stock);
+    printf("name: %-20s brand: %-20s model: %-20s price: %-5.2lf stock: %-5s\n", lista->data.name, lista->data.brand, lista->data.model, lista->data.price, lista->data.stock);
     lista = lista->next;
   }
 }
 
+void swap(node *a, node *b)
+{
+    ElectronicItem temp = a->data;
+    a->data = b->data;
+    b->data = temp;
+}
+
+void to_lower(char *name)
+{
+    int i = 0;
+    while (name[i])
+    {
+        name[i] = tolower(name[i]);
+        i++;
+    }
+}
+
+void sort_list(node **lista, int field) {
+  if(field!= NAME_COMPARE && field !=BRAND_COMPARE && field != PRICE_COMPARE) {
+        printf("Error: Invalid field.\n");
+        return;
+  }
+
+  node *current = *lista;
+
+  while (current->prev != NULL) {
+    current = current->prev;
+  }
+
+  int swapped;
+  node *ptr1;
+  node *lptr = NULL;
+
+  if (current == NULL)
+    return;
+
+  do
+  {
+    swapped = 0;
+    ptr1 = current;
+
+    while (ptr1->next != lptr)
+    {
+        if (field == NAME_COMPARE) {
+            char name1[20], name2[20];
+            if(strlen(ptr1->data.name)>=19) 
+                strncpy(name1, ptr1->data.name,19);
+            else
+                strcpy(name1, ptr1->data.name);
+            if(strlen(ptr1->next->data.name)>=19) 
+                strncpy(name2, ptr1->next->data.name,19);
+            else
+                strcpy(name2, ptr1->next->data.name);
+            to_lower(name1);
+            to_lower(name2);
+            if (strcmp(name1, name2) > 0)
+            {
+                swap(ptr1, ptr1->next);
+                swapped = 1;
+            }
+        } else if (field== BRAND_COMPARE) {
+            char brand1[20], brand2[20];
+            if(strlen(ptr1->data.brand)>=19) 
+                strncpy(brand1, ptr1->data.brand,19);
+            else
+                strcpy(brand1, ptr1->data.brand);
+            if(strlen(ptr1->next->data.brand)>=19) 
+                strncpy(brand2, ptr1->next->data.brand,19);
+            else
+                strcpy(brand2, ptr1->next->data.brand);
+            to_lower(brand1);
+            to_lower(brand2);
+            if (strcmp(brand1, brand2) > 0)
+            {
+                swap(ptr1, ptr1->next);
+                swapped = 1;
+            }
+        } else {
+            float price1=0; float price2=0;
+                price1 = ptr1->data.price;
+                price2 = ptr1->next->data.price;
+
+            if (price1<price2)
+            {
+                swap(ptr1, ptr1->next);
+                swapped = 1;
+            }
+        }
+
+        ptr1 = ptr1->next;
+    }
+    lptr = ptr1;
+  } while (swapped);
+}
+
+void search_name(node *lista, char search[20])
+{
+    scanf("%s",search);
+    node *current = get_first(lista);
+    int i;
+    to_lower(search);
+    while (current != NULL)
+    {
+        char name_temp[20],brand_temp[20];
+        strcpy(name_temp,current->data.name);
+        strcpy(brand_temp,current->data.brand);
+        to_lower(name_temp);
+        to_lower(brand_temp);
+        
+        if (strstr(name_temp, search) != NULL || strstr(brand_temp, search) != NULL)
+        {
+            printf("found:  ");
+            printf("name: %-20s brand: %-20s model: %-20s price: %-5.2lf stock: %-5s\n", lista->data.name, lista->data.brand, lista->data.model, lista->data.price, lista->data.stock);
+        }
+        current = current->next;
+    }
+}
 
 int main()
-{node *lista = NULL;ElectronicItem dane;
+{node *lista = NULL;ElectronicItem dane;char name[20];
     read_from_csv(&lista);
+   // print_list(lista);
+    sort_list(&lista,NAME_COMPARE);
+    printf("\n\n\n");
     print_list(lista);
+    search_name(lista,name);
  printf("siema");
 }
